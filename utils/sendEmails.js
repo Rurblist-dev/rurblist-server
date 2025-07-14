fbce581ed4cd58ca5b283c2ee7e899f33893fb80
Dave <uchennadavid2404@gmail.com>
2025-07-14 01:39:17 +0100

    Configure email to use custom domain

--- utils/sendEmails.js
+++ utils/sendEmails.js
@@ -1,22 +1,22 @@
-const ZeptoMail = require("@netcore/zeptomail-node");
+// const ZeptoMail = require("@netcore/zeptomail-node");
 
-const zeptoClient = new ZeptoMail({
-  apiKey: process.env.ZEPTOMAIL_API_KEY, // Store your API key securely
-});
+// const zeptoClient = new ZeptoMail({
+//   apiKey: process.env.ZEPTOMAIL_API_KEY, // Store your API key securely
+// });
 
-async function sendEmail(to, subject, htmlContent) {
-  try {
-    const response = await zeptoClient.sendEmail({
-      from: "yourverifiedemail@yourdomain.com", // Must be verified in ZeptoMail
-      to,
-      subject,
-      html: htmlContent,
-    });
+// async function sendEmail(to, subject, htmlContent) {
+//   try {
+//     const response = await zeptoClient.sendEmail({
+//       from: "yourverifiedemail@yourdomain.com", // Must be verified in ZeptoMail
+//       to,
+//       subject,
+//       html: htmlContent,
+//     });
 
-    console.log(`Email sent to ${to}: Message ID ${response.messageId}`);
-  } catch (error) {
-    console.error("Failed to send email via ZeptoMail:", error);
-  }
-}
+//     console.log(`Email sent to ${to}: Message ID ${response.messageId}`);
+//   } catch (error) {
+//     console.error("Failed to send email via ZeptoMail:", error);
+//   }
+// }
 
-module.exports = sendEmail;
+// module.exports = sendEmail;
